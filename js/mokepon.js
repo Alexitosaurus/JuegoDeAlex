526ee97f4b0fefea25b1b58a11d6742a71f7aea2
Alex Daniel <alexitosauriorex@hotmail.com>
2026-06-16 00:25:12 -0600

    Version funcional con movimiento en mapa

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,9 +1,20 @@
+
 let sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
 let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
 let sectionBotonReiniciar = document.getElementById("reiniciar")
 let mascotases = document.getElementById("mascotases")
 
-  
+
+
+
+let sectionVerMapa = document.getElementById("ver-mapa")
+let mapa = document.getElementById("mapa")
+
+let BotonMoverDerecha = document.getElementById("mover-Derecha")
+let BotonMoverIzquierda = document.getElementById("mover-Izquierda")
+let BotonMoverArriba = document.getElementById("mover-Arriba")
+let BotonMoverAbajo = document.getElementById("mover-Abajo")
+
 let opcionDeMokepones 
    
 
@@ -13,7 +24,11 @@ let ataqueEnemigo
 let vidasJugador = 3
 let vidasEnemigo = 3
 
-    
+let intervalo
+
+let mapaBackground = new Image()
+mapaBackground.src = './assets/plaza.jpg'
+
 
 let mokepones = []
 
@@ -24,6 +39,15 @@ class Mokepon {
         this.foto = foto 
         this.vida = vida
         this.ataques = []
+        this.x = 20
+        this.y = 20
+        this.ancho = 80
+        this.alto = 80
+        this.mapaFoto = new Image()
+        this.mapaFoto.src = foto
+        this.VelocidadY = 0
+        this.VelocidadX = 0
+        
     }
 }
 
@@ -34,6 +58,10 @@ let GatoMarciano = new Mokepon('GatoMarciano', './assets/marcianillo.png',5 )
 let cuche = new Mokepon('Cuche','./assets/cochino.png', 5 )
 
 let Mapache = new Mokepon('Mapache','./assets/mapache.png', 5 )
+
+let lienzo = mapa.getContext("2d")
+
+
 
 //mokepones.push(GatoMarciano, cuche, Mapache)
 
@@ -75,7 +103,11 @@ cuche.ataques.push(
 
 mokepones.push(GatoMarciano,cuche,Mapache)
 
+
+
 function iniciarJuego() {
+
+
 
 mokepones.forEach((mokepon) => {
     
@@ -92,6 +124,10 @@ mokepones.forEach((mokepon) => {
 
  
     sectionSeleccionarAtaque.style.display = 'none'
+  
+  
+  
+    sectionVerMapa.style.display = 'none' //la acabo de agregar
 
 
  let caballitos = [] 
@@ -115,18 +151,18 @@ caballitos.push(cajasauria.value)
 }
 
 
-    sectionSeleccionarAtaque.style.display = 'none'
 
 
 
     
     
         
-sectionSeleccionarAtaque.style.display = 'none'
+//sectionSeleccionarAtaque.style.display = 'none'
 
 sectionBotonReiniciar.style.display = "none"
 
-    
+
+
 
      
 
@@ -147,11 +183,18 @@ botonReiniciar.addEventListener('click', reiniciarJuego)
 
 function seleccionarMascotaJugador() {
 
+
+    sectionSeleccionarMascota.style.display = "none"
     
+    sectionVerMapa.style.display = 'flex'
 
+    iniciarMapa()
+    //lienzo.fillRect(20,10,100,100) 
+pintarCanvas ()
 
-   
-    sectionSeleccionarAtaque.style.display = 'none'
+     //la acabo de agregar
+
+    sectionSeleccionarAtaque.style.display = "none" //le puse block
 
 let imputHipodogue = document.getElementById('GatoMarciano') //  con esto puedo poner codigo aqui y no meterlo al condicional del if para en el if tener la logica basica 
 let imputCapipepo = document.getElementById('Cuche')
@@ -173,16 +216,26 @@ let spanMascotaJugador = document.getElementById('mascota-jugador')
 
 }
 
-
+   
 seleccionarMascotaEnemigo()
 
 }
 
 function seleccionarMascotaEnemigo() {
 
-    sectionSeleccionarMascota.style.display = "none"
+    
 
-        sectionSeleccionarAtaque.style.display = 'block'
+    
+
+    sectionSeleccionarAtaque.style.display = 'none' // aqui quite la seccion le puse none y estaba en block
+
+
+
+
+
+
+
+
 
 
     let mascotaAleatoria = aleatorio(0,mokepones.length - 1) 
@@ -255,13 +308,13 @@ function ataqueAleatorioEnemigo () {
 
         if (vidasJugador== 0) {
             crearMensajeFinal("HAS PERDIDOOOO!!!!")
-           
+          
         } else if (vidasEnemigo == 0) {
             crearMensajeFinal("FELICITACIONES GANASTE!!!!")
         }
 
     }   
-
+//BotonMoverDerecha.addEventListener('click', moverGatoMarciano) anomalia rara
 
  function crearMensaje(resultado) {
     let sectionMensajes = document.getElementById('mensajes')
@@ -309,6 +362,113 @@ botonTierra.disabled = true
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+
+function pintarCanvas () {
+    GatoMarciano.y = GatoMarciano.y + GatoMarciano.VelocidadY
+    GatoMarciano.x = GatoMarciano.x + GatoMarciano.VelocidadX
+    lienzo.clearRect(0, 0, mapa.width, mapa.height)
+    lienzo.drawImage(
+        mapaBackground,
+        0,
+        0,
+        mapa.width,
+        mapa.height
+    )
+    lienzo.drawImage(
+    GatoMarciano.mapaFoto,
+    GatoMarciano.x,
+    GatoMarciano.y,
+    GatoMarciano.ancho,
+    GatoMarciano.alto
+    )
+ 
+}
+
+
+
+
+function moverGatoDerecha () {
+
+    GatoMarciano.VelocidadX = 5
+
+// GatoMarciano.x = GatoMarciano.x + 5
+pintarCanvas()
+
+}
+
+
+function moverGatoIzquierda () {
+
+     GatoMarciano.VelocidadX = - 5
+// GatoMarciano.x = GatoMarciano.x - 5
+ pintarCanvas()
+ 
+
+
+}
+function moverGatoArriba () {
+
+    GatoMarciano.VelocidadY = - 5
+// GatoMarciano.x = GatoMarciano.x - 5
+pintarCanvas()
+
+}
+
+function moverGatoAbajo () {
+
+    GatoMarciano.VelocidadY = 5
+pintarCanvas()
+
+}
+
+function detenerMovimiento () {
+
+    GatoMarciano.VelocidadX = 0
+    GatoMarciano.VelocidadY = 0
+
+}
+
+function sePresionoUnaTecla(event) {
+
+switch (event.key) {
+        case 'ArrowUp':
+        moverGatoArriba()
+        break;
+        case 'ArrowDown':
+        moverGatoAbajo()
+        break;
+        case 'ArrowRight':
+        moverGatoDerecha()
+        break;
+        case 'ArrowLeft':
+        moverGatoIzquierda()
+        break;
+
+    default:
+ 
+    
+    break;
+    
+}
+    
+//console.log(event.key);
+
+}
+
+function iniciarMapa() {
+
+    mapa.width = 800
+    mapa.height = 600 
+
+    window.addEventListener('keydown',sePresionoUnaTecla)
+    window.addEventListener('keyup',detenerMovimiento)
+
+    intervalo = setInterval(pintarCanvas, 50)
+
+}
+
+
 
 window.addEventListener('load', iniciarJuego)
 
